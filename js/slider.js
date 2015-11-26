4cffb1f6e383b1db404cb9e4af9af82032101f79
David Reinisch <david.reinisch@tastenwerk.com>
2015-11-26 13:22:01 +0100

    basic slider

---
+++ js/slider.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  if( $('#main-slider img').length > 1 ){
+    $('#main-slider').tinycarousel({ 
+      interval: true,
+      intervalTime: 5000
+    });
+    console.log('inside');
+  }
+});
